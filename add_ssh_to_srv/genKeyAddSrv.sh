7620eabf2c1e2c8eedfcd048be5daddced8a8f92
Hoper <41995992+DenisPlastinin@users.noreply.github.com>
2019-04-29 17:58:58 +0300

    Update genKeyAddSrv.sh

--- add_ssh_to_srv/genKeyAddSrv.sh
+++ add_ssh_to_srv/genKeyAddSrv.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 Key_Name="user_ssh_key"
+# generate key 
 /usr/bin/ssh-keygen -P '' -t rsa -f ~/.ssh/$Key_Name
+# add key to host 
+# need to install sshpass "sudo apt install sshpass"
+# sshpass -p "password" ssh -o StrictHostKeyChecking=no -i ../.ssh/user_ssh_key user@host
